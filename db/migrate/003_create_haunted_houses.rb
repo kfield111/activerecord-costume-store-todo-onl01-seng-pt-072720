e19bcc8339927ddd36265bf168b80cd4f7ea949d
Kelin Field <kfield111@hotmail.com>
2020-10-18 01:51:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.text :description
+      t.string :description
     end
   end
 end
