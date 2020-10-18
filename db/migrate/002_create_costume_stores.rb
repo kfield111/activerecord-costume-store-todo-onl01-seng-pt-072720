2afa1f6cf1ca1b3a438c13ace3e7c3b5a7526f8e
Kelin Field <kfield111@hotmail.com>
2020-10-18 01:25:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.name :string
+      t.location :string
+      t.costume_inventory :integer
+      t.number_of_employees :integer
+      t.in_business :boolean
+      t.opening_time :integer
+      t.closing_time :integer
+    end
+  end
+end
