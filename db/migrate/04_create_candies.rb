c1a6006aa352b0c84dd477a0992cfa2306b4d4a2
Kelin Field <kfield111@hotmail.com>
2020-10-18 01:39:37 +0000

    Automatically backed up by Learn

--- db/migrate/04_create_candies.rb
+++
@@ -1,9 +0,0 @@
-class CreateCandies < ActiveRecord::Migration[5.1]
-  def change
-    create_table :candies do |t|
-      t.name :string
-      t.integer :calories
-      t.timestamps
-    end
-  end
-end
