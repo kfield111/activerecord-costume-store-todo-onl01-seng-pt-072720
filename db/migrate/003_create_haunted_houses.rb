fb48468e6444ee90d4f3b821a07f17d8efabe517
Kelin Field <kfield111@hotmail.com>
2020-10-18 01:30:47 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class HauntedHouses < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.name :string
