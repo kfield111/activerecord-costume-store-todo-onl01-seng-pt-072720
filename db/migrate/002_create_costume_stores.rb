80e641ceb69264a227bac5233065a7d4593e62d4
Kelin Field <kfield111@hotmail.com>
2020-10-18 01:37:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
-      t.name :string
-      t.location :string
-      t.costume_inventory :integer
-      t.number_of_employees :integer
-      t.in_business :boolean
-      t.opening_time :datetime
-      t.closing_time :datetime
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
